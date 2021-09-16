344711f04d7b50ed16edff9f4ff11e938459bd57
darkg123 <sgogoi628@gmail.com>
2021-09-16 18:48:28 +0530

    remove icon added

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MovieControls } from './MovieControls';
+
 
 export const MovieCard = ({ movie, type }) => {
   return (
@@ -13,6 +15,8 @@ export const MovieCard = ({ movie, type }) => {
         <div className="filler-poster">
         </div>
       )}
+
+      <MovieControls type={type} movie={movie} />
     </div>
   )
 }
--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -2,12 +2,19 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
 export const ResultCard = ({ movie }) => {
-  const { addMovieToWatchlist, watchlist } = useContext(GlobalContext);
+  const { addMovieToWatchlist, addMovieToWatched, watchlist, watched } = useContext(GlobalContext);
 
 
   let storedMovie = watchlist.find(o => o.id === movie.id);
+  let storedMovieWatched = watched.find((o) => o.id === movie.id)
 
-  const watchlistDisabled = storedMovie ? true : false;
+  const watchlistDisabled = storedMovie
+    ? true
+    : storedMovieWatched
+      ? true
+      : false;
+
+  const watchedDisabled = storedMovieWatched ? true : false;
 
   return (
     <div className="result-card">
@@ -31,7 +38,15 @@ export const ResultCard = ({ movie }) => {
         <div className="controls">
           <button className="btn"
             disabled={watchlistDisabled}
-            onClick={() => addMovieToWatchlist(movie)}>Add to Watchlist</button>
+            onClick={() => addMovieToWatchlist(movie)}>
+            Add to Watchlist
+          </button>
+
+          <button className="btn"
+            disabled={watchedDisabled}
+            onClick={() => addMovieToWatched(movie)}>
+            Add to Watched
+          </button>
         </div>
       </div>
     </div>
